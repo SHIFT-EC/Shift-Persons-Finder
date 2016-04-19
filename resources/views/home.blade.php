c7d6d67da22c201c1c6efc56909f8b148ee10433
Javier Montalvo <giojavi04@gmail.com>
2016-04-19 10:21:40 -0500

    correct a letter

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.template')
 
-@section('title','Emargencias Ecuador')
+@section('title','Emergencias Ecuador')
 
 @section('content')
     <h2 class="title-persons">Últimas personas agregadas</h2>
