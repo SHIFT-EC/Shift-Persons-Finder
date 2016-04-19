ff48e989cfa76cebbeca03d1025575d0f71bae97
tavo1987 <tavo198718@gmail.com>
2016-04-19 12:23:40 -0500

    add donation information

--- resources/views/partials/banner-donation.blade.php
+++ resources/views/partials/banner-donation.blade.php
@@ -15,17 +15,100 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title" id="myModalLabel">Lugares</h4>
+                    <h4 class="modal-title" id="myModalLabel"><strong>Donaciones</strong></h4>
                 </div>
-                <div class="modal-body">
-                    <h4>Quito</h4>
-                    <ul>
-                        <li>item1</li>
-                        <li>item2</li>
-                        <li>item3</li>
-                        <li>item4</li>
-                        <li>item5</li>
+                <div class="modal-body text-justify">
+                    <p>
+                        El gobierno central, a través del MIES, ha habilitado los “Puntos de Solidaridad”,
+                        ubicados en todas las oficinas de las Direcciones Distritales MIES y Coordinaciones
+                        Zonales MIES, que trabajarán desde este domingo desde las 08:00 hasta las 17:00.
+                    </p>
+
+                    <h4><strong>Recepción de donaciones</strong></h4>
+                    <p><strong>Parque Samanes:</strong> Vía Paseo del Parque y Av. Francisco de Orellana, de 08:00 a 20:00.</p>
+
+                    <p><strong>Edificio Joaquín Gallegos Lara:</strong> Av. Carlos Luis Plaza Dañín frente al Centro Comercial Policentro, de 07:00 a 18:00.</p>
+
+                    <p><strong>Centro de Atención Ciudadana CAC Santa Elena:</strong>Cdla. Santa Paula, Av. Carlos Espinoza Larrea y Vía a Salinas, de 08:00 a 17:00.</p>
+
+                    <p><strong>Centro de Atención Ciudadana Latacunga:</strong> Plaza Cultural, Sánchez de Orellana entre Quito y Marquéz de Maenza, 08:00 a 20:00.</p>
+
+                    <h4><strong>Municipio de Quito:</strong></h4>
+
+                    <p>En Quito, funcionarios del Distrito Metropolitano informan que se reciben donaciones en la</p>
+                    
+                    <p>
+                        <strong>Arquidiócesis de Quito</strong>
+                    </p>
+
+                    <p>Diferentes Iglesias de la ciudad</p>
+
+                    <p><strong>Patronato San José:</strong> ubicado en la calle Benalcázar y Chile</p>
+
+                    <p>
+                        <strong>Cruz del Papa:</strong> parque La Carolina; se abrirán también otros puntos, que se informarán de manera oportuna, posteriormente, informó el municipio
+                    </p>
+
+                    <h4><strong>Donaciones económicas: </strong></h4>
+
+                    <p>La Cancillería de Ecuador ha abierto cuentas bancarias, una en el territorio nacional y dos en el extranjero para recibir donaciones económicas.</p>
+
+                    <p>Cuentas para contribuciones nacionales:</p>
+
+                    <ul class="list">
+                        <li><strong>Entidad:</strong>Banco Nacional de Fomento</li>
+                        <li><strong>Nombre de cuenta::</strong>“BCE Secretaria de Gestión de Riesgos”,</li>
+                        <li><strong>Número de Cuenta corriente:</strong> 3001033189</li>    
+                        <li><strong>Código de ingresos:</strong> 180110</li>
                     </ul>
+                    <br><br>
+                     
+                    <ul>
+                        <li><strong>Entidad:</strong> Banco Central del Ecuador</li>
+                        <li><strong>Número de cuenta:</strong> 01121807</li>
+                        <li><strong>Nombre de la cuenta:</strong> "CCU Secretaría de Gestión Riesgos/Planta Central"</li>
+                        <li>(<strong>RUC de la Secretaría de Gestión de Riesgos:</strong> 1768142760001)</li>
+                        
+                    </ul>
+
+                    <br>
+                    <h4><strong>Cuentas para contribuciones Internacionales:</strong></h4>
+                    <br>
+                    <strong>Opción 1</strong><br>
+                    
+                    <br>
+                    <p><strong>Transferencia SWIFT</strong></p>
+
+                    <ul>
+                        <li><strong>Beneficiario:</strong> Banco Central del Ecuador</li>
+                        <li>Futuro Crédito cuenta N° 01121807 “CCU SECRETARIA GESTIÓN RIESGOS/PLANTA CENTRAL”</li>
+                        <li><strong>Código SWIFT: </strong>BCENECEQ</li>
+                        <li><strong>Banco del benficiario:</strong> Fondo Latinoamericano de Reservas (FLAR)</li>
+                        <li><strong>Código SWIFT:</strong> FLARCOBB</li>
+                        <li><strong>Número de cuenta BCE en FLAR:</strong> 10602</li>
+                        <li><strong>Banco intermediario:</strong> Bank of América NA</li>
+                        <li><strong>Código SWIFT:</strong> BOFAUS3N</li>
+                        <li><strong>Número de cuenta FLAR en Bank of America:</strong> 6550139851</li>
+                    </ul>
+                    
+                    <br>
+                    <strong>Opción 2</strong>
+                    
+                    <br>
+                    <p><strong>Transferencia SWIFT</strong></p>
+
+                    <ul>
+                        <li><strong>Banco:</strong> Bank of America NY</li>
+                        <li><strong>Cuenta:</strong> 3751560139</li>
+                        <li><strong>Coordenadas:</strong> BANK type ZBA bank</li>
+                        <li><strong>Bank id</strong> 111000012</li>
+                        <li><strong>Denominado Account name:</strong> UNDP REP IN ECUADOR $ ACCT</li>
+                    </ul>
+
+                    <p>
+                        También, el Ministerio de Educación informó que a partir de este lunes se receptará donaciones en las instituciones educativas de Ecuador.
+                    </p>
+
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
