e6eb06077331b12a636bc15d443058444f898ffc
Edwin Ramírez <tavo198718@gmail.com>
2016-04-18 22:54:50 -0500

    Add style and structure for banner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,6 @@
 @section('title','home')
 
 @section('content')
-    <div class="row">
-        <div class="col-12">
-            <h2>Contenido</h2>
-        </div>
-    </div>
-@endsection
-
-@section('scripts')
 
 @endsection
 
