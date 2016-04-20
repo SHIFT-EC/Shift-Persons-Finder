c86018958569ac3b1a9d8357ccb012ea5a94a9a3
tavo1987 <tavo198718@gmail.com>
2016-04-20 10:26:57 -0500

    Change text for share twitter

--- resources/views/partials/networks.blade.php
+++ resources/views/partials/networks.blade.php
@@ -1,4 +1,4 @@
 <div class="networks">
     <a href="https://www.facebook.com/sharer/sharer.php?u=https://emergenciaecuador.com" class="icon-facebook-square"> Compartir</a>
-    <a href="https://twitter.com/intent/tweet?url=https://emergenciaecuador.com&text=Encuentrálos%20por%20" class="icon-twitter-square"> Compartir</a>
+    <a href="https://twitter.com/intent/tweet?url=https://emergenciaecuador.com&text=Reporta%20desaparecidos%20por%20" class="icon-twitter-square"> Compartir</a>
 </div>
